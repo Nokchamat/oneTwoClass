326c496596e25bed9d62ba4434e832c2d459641e
NokchaMat <rkdtjd976@gmail.com>
2023-06-14 13:45:13 +0900

    init

---
+++ src/main/java/com/onetwoclass/onetwoclass/OneTwoClassApplication.java
@@ -0,0 +1,13 @@
+package com.onetwoclass.onetwoclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTwoClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneTwoClassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onetwoclass/onetwoclass/OneTwoClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onetwoclass.onetwoclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneTwoClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
