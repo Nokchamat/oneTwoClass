ea2f00c8b3d749c1b3872046f69dd2984e215a30
Nokchamat <107979129+Nokchamat@users.noreply.github.com>
2023-06-21 14:33:26 +0900

    Update Chatting.java
    text에 @Lob 추가

--- src/main/java/com/onetwoclass/onetwoclass/domain/Chatting.java
+++ src/main/java/com/onetwoclass/onetwoclass/domain/Chatting.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
@@ -21,6 +22,7 @@ public class Chatting {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Lob
   private String text;
 
   @ManyToOne
